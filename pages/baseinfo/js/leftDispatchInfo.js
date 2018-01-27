c49360353962e3f74af98f9720d748dae5fd12c0
1107323889 <1107323889@qq.com>
2018-01-27 13:47:48 +0800

    权限页面代码提交

---
+++ pages/baseinfo/js/leftDispatchInfo.js
@@ -0,0 +1,172 @@
+
+var PageLeftDispatchInfo = function(){
+    return {
+        defaultOption: {
+            basePath:"",
+            leftDispatchInfoGrid : null
+        },
+        init :function ()
+        {
+            mini.parse();
+            this.basePath = PageMain.basePath;
+            this.leftDispatchInfoGrid = mini.get("leftDispatchInfoGrid");
+            this.leftDispatchInfoGrid.setUrl(PageMain.defaultOption.httpUrl + "/leftDispatchInfo/getList");
+            this.funSearch();
+        },
+        funSearch : function()
+        {
+            var leftDispatchInfoForm = new mini.Form("leftDispatchInfoForm");
+            this.leftDispatchInfoGrid.load(leftDispatchInfoForm.getData());
+        },
+        funReset : function()
+        {
+            var leftDispatchInfoForm = new mini.Form("leftDispatchInfoForm");
+            leftDispatchInfoForm.setData();
+            mini.get("queryParamFlag").setValue("1");
+            this.leftDispatchInfoGrid.load(leftDispatchInfoForm.getData());
+        },
+        funAdd : function()
+        {
+            var paramData = {action: "add", row:{}, title:"新增数据"};
+            this.funOpenInfo(paramData);
+        },
+        funModify : function()
+        {
+            var row = this.leftDispatchInfoGrid.getSelected();
+            if(row)
+            {
+                var paramData = {action: "modify", row: row, title:"编辑数据"};
+                this.funOpenInfo(paramData);
+            }
+            else
+            {
+                PageMain.funShowMessageBox("请选择一条记录");
+            }
+        },
+        funOperRenderer : function(e)
+        {
+            return '<a class="mini-button-icon mini-iconfont icon-detail" style="display: inline-block;  height:16px;padding:0 10px;" title="详情查看" href="javascript:PageLeftDispatchInfo.funDetail()"></a>';
+        },
+        funDetail : function()
+        {
+            var row = this.leftDispatchInfoGrid.getSelected();
+            var paramData = {action: "oper", row:row, title:"查看详细"};
+            this.funOpenInfo(paramData);
+        },
+        funOpenInfo : function(paramData)
+        {
+            var me = this;
+            mini.open({
+                url: PageMain.funGetRootPath() + "/pages/baseinfo/leftDispatchInfo_add.html",
+                title: paramData.title,
+                width: 650,
+                height: 30 *  11 + 65,
+                onload:function(){
+                    var iframe=this.getIFrameEl();
+                    iframe.contentWindow.PageLeftDispatchInfoAdd.funSetData(paramData);
+                },
+                ondestroy:function(action){
+                    me.leftDispatchInfoGrid.reload();
+                }
+            })
+        },
+        funDelete : function()
+        {
+            var row = this.leftDispatchInfoGrid.getSelected();
+            var me = this;
+            if(row)
+            {
+                mini.confirm("确定要删除这条记录?", "提醒", function (action) {
+                    if (action == "ok")
+                    {
+                        $.ajax({
+                            url : PageMain.defaultOption.httpUrl + "/leftDispatchInfo/del",
+                            type: 'POST',
+                            data: {"id": row.id},
+                            dataType: 'json',
+                            success: function (data)
+                            {
+
+                                if (data.success)
+                                {
+                                    mini.alert("操作成功", "提醒", function(){
+                                        if(data.success)
+                                        {
+                                            me.leftDispatchInfoGrid.reload();
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    PageMain.funShowMessageBox(data.msg);
+                                }
+                            },
+                            error: function ()
+                            {
+                                PageMain.funShowMessageBox("删除记录失败");
+                            }
+                        });
+                    }
+                })
+            }
+            else
+            {
+                mini.alert("请先选择要删除的记录");
+            }
+        }
+    }
+}();
+
+
+function formatData (e) {
+    var field = e.field;
+    var value = e.value;
+    var result = "--";
+
+    //转换"创建时间"
+    if("loadingTime" == field || "bookSTime" == field ||"bookETime" == field){
+        var createdTime = new Date(1000 * value);
+        var year = createdTime.getFullYear();
+        var mon = createdTime.getMonth() + 1;
+        if(10 > mon){
+            mon = "0" + mon;
+        }
+
+        var sDate = createdTime.getDate();
+        if(10 > sDate){
+            sDate = "0" + sDate;
+        }
+
+        var hours = createdTime.getHours();
+        if(10 > hours){
+            hours = "0" + hours;
+        }
+
+        var min = createdTime.getMinutes();
+        if(10 > min){
+            min = "0" + min;
+        }
+
+        var sec = createdTime.getSeconds();
+        if(10 > sec){
+            sec = "0" + sec;
+        }
+
+        result = year + "-" + mon + "-" + sDate + "  " + hours + ":" + min + ":" + sec;
+    }
+    //转换"状态"
+    if("status" == field){
+        if(1 == e.value){
+            result = "待发布";
+        }else if(2 == e.value){
+            result = "已发布";
+        }else if(3 == e.value){
+            result = "已取消";
+        }
+    }
+    return result;
+}
+
+$(function(){
+    PageLeftDispatchInfo.init();
+});
---
+++ pages/systeminfo/js/dataDictionary_add.js
@@ -0,0 +1,85 @@
+
+var PageDataDictionaryAdd = function(){
+    return {
+        defaultOption: {
+            basePath:"",
+            action : "",
+            dataDictionaryForm : null
+
+        },
+        init :function ()
+        {
+            mini.parse();
+            this.basePath = PageMain.basePath;
+            this.dataDictionaryForm = new mini.Form("dataDictionaryFormAdd");
+            // mini.get("gender").setData([{id:0,name:"未知"},{id:1, name:"男"},{id:2, name:"女"}])
+        },
+        funSetData : function(data)
+        {
+            var row = data.row;
+            this.action = data.action;
+            this.dataDictionaryForm.setData(row);
+            if(this.action == "oper")
+            {
+                mini.get("layout_dataDictionary_add").updateRegion("south", { visible: false });//$(".mini-toolbar").hide();
+                var fields = this.dataDictionaryForm.getFields();
+                for (var i = 0, l = fields.length; i < l; i++)
+                {
+                    var c = fields[i];
+                    if (c.setReadOnly) c.setReadOnly(true);     //只读
+                    if (c.setIsValid) c.setIsValid(true);      //去除错误提示
+                }
+            }
+        },
+        funSave : function()
+        {
+            this.dataDictionaryForm.validate();
+            if (!this.dataDictionaryForm.isValid())
+            {
+                var errorTexts = form.getErrorTexts();
+                for (var i in errorTexts)
+                {
+                    mini.alert(errorTexts[i]);
+                    return;
+                }
+            }
+
+            var me = this;
+            var obj = this.dataDictionaryForm.getData(true);
+            $.ajax({
+                url : PageMain.defaultOption.httpUrl + "/dataDictionary/" + me.action + "?a="+Math.random(),
+                type : 'POST',
+                data : obj,
+                dataType: 'json',
+                success: function (data)
+                {
+                    if (data.success)
+                    {
+                        mini.alert("操作成功", "提醒", function(){
+                            if(data.success)
+                            {
+                                PageMain.funCloseWindow("save");
+                            }
+                        });
+                    }
+                    else
+                    {
+                        PageMain.funShowMessageBox(data.msg);
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown)
+                {
+                    PageMain.funShowMessageBox("操作出现异常");
+                }
+            });
+        },
+        funCancel : function()
+        {
+            PageMain.funCloseWindow("cancel");
+        }
+    }
+}();
+
+$(function(){
+    PageDataDictionaryAdd.init();
+});
