b91406cd42645fe80b9a379bb7bac8a5db24c2aa
litao <TeamMicLee@foxmail.com>
2018-01-28 12:56:01 +0800

    运力调度订单提交操作修改

--- pages/jk/js/capcitySchedul.js
+++ pages/jk/js/capcitySchedul.js
@@ -44,7 +44,7 @@ var CapcitySchedul = function(){
                 field = e.field,
                 value = e.value;
                 //设置行样式
-                if (record.status == 3) {
+                if (record.status == 2) {
                     e.rowStyle = "background: #fceee2 !important;";
                 }
             });
@@ -84,6 +84,14 @@ var CapcitySchedul = function(){
             var customerTaskFlowData = this.orderGrid.getSelected();
             var shipListForm = new mini.Form("shipListForm");
             var searchParam = shipListForm.getData();
+            var shipTypeList = $("input[name='shipType']:checked");
+            var shipType = '';
+            if (shipTypeList.length > 0) {
+                for (var i =0; i < shipTypeList.length; i++) {
+                    shipType += shipTypeList[i].value + ",";
+                }
+                searchParam.shipType = shipType;
+            }
             if (customerTaskFlowData) {
                 searchParam.customerTaskFlowId = customerTaskFlowData.id;
             }
@@ -106,18 +114,6 @@ var CapcitySchedul = function(){
                 //orderDetailsGrid.load({customerTaskFlowId: record.id, "queryParamFlag": 1});
             }
         },
-        funRendererPortType : function (e)
-        {
-            if (e.value == 1)
-            {
-                return "集装箱"
-            }
-            else if (e.value == 2)
-            {
-                return "其它"
-            }
-            return e.value;
-        },
         funOrderOperRenderer : function(e)
         {
             var grid = e.sender;
@@ -127,11 +123,13 @@ var CapcitySchedul = function(){
             var s = '<a class="Blue_Button" href="javascript:CapcitySchedul.funDelRow(\'' + uid + '\')">删除</a> '
                     + '<span class="separator"></span>'
                     + '<a class="Blue_Button" href="javascript:CapcitySchedul.funReDelRow(\'' + uid + '\')">取消删除</a> ';
-            if (e.row.dispatchInfoId) {
-                return s;
+            if (e.row.id && e.row.status == 0) {
+                return '<a class="Blue_Button" href="javascript:CapcitySchedul.funDelRow(\'' + uid + '\')">删除</a> ';
                 //return '<input type="radio" name="deleteOrNot" /><label>删除</label><input type="radio" name="deleteOrNot" /><label>取消删除</label>';
             }
-            else {
+            else if (e.row.id && e.row.status == 2) {
+                return '<a class="Blue_Button" href="javascript:CapcitySchedul.funReDelRow(\'' + uid + '\')">取消删除</a>';
+            } else {
                 return '';
             }
             //return '<a class="mini-button-icon mini-iconfont icon-remove" style="display: inline-block;  height:16px;padding:0 10px;" title="删除" href="javascript:CapcitySchedul.funRemoveRow()"></a>';
@@ -140,8 +138,19 @@ var CapcitySchedul = function(){
         {
             return PageMain.funStrToDate(e.row.loadingTime);
         },
+        funOrderDetailsTimeRenderer: function(e)
+        {
+            if (typeof(e.row.preArriveTime) == "number") {
+                return PageMain.funStrToDate(e.row.preArriveTime);
+            } else {
+                return mini.formatDate(e.row.preArriveTime, "yyyy-MM-dd HH:mm:ss");
+            }
+            // if (e.row.id) {
+            //     return PageMain.funStrToDate(e.row.preArriveTime);
+            // }
+        },
         funStatusRenderer: function(e) {
-            if (e.row.dispatchInfoId) {
+            if (e.row.id) {
                 return '0';
             }
             else {
@@ -150,7 +159,7 @@ var CapcitySchedul = function(){
         },
         funOnCellBeginEdit: function(e) {
             var record = e.record, field = e.field;
-            if (record.status == "3") {
+            if (record.status == "2") {
                 e.cancel = true;    //如果置为删除状态则不允许编辑
             }
             if (field == "shipId" || field == "shipType" || field == "preWeight" || field == "preSettleAmount") {
@@ -162,15 +171,15 @@ var CapcitySchedul = function(){
             var record = e.record;
 
             if (e.field == "preSettleAmount") {
-                var actualPrice = record.actualPrice;
+                var actualTransferPrice = record.actualTransferPrice;
                 var preLoad = record.preLoad;
-                if (actualPrice == undefined || preLoad == undefined) {
+                if (actualTransferPrice == undefined || preLoad == undefined) {
                     e.cellHtml = 0;
                     e.record.preSettleAmount = 0;
                 } else {
-                    //e.cellHtml = actualPrice * preLoad;
-                    e.cellHtml = CapcitySchedul.funNumMulti(actualPrice, preLoad);
-                    e.record.preSettleAmount = CapcitySchedul.funNumMulti(actualPrice, preLoad);
+                    //e.cellHtml = actualTransferPrice * preLoad;
+                    e.cellHtml = CapcitySchedul.funNumMulti(actualTransferPrice, preLoad);
+                    e.record.preSettleAmount = CapcitySchedul.funNumMulti(actualTransferPrice, preLoad);
                 }
             }
         },
@@ -184,7 +193,7 @@ var CapcitySchedul = function(){
                 var total = 0;
                 for (var i = 0, l = rows.length; i < l; i++) {
                     var row = rows[i];
-                    var t = row.actualPrice * row.preLoad;
+                    var t = row.actualTransferPrice * row.preLoad;
                     if (isNaN(t)) continue;
                     total += t;
                 }
@@ -196,7 +205,8 @@ var CapcitySchedul = function(){
         {
             var row = orderDetailsGrid.getRowByUID(row_uid);
             if (row) {
-                orderDetailsGrid.updateRow(row, {status: 3});
+                var rowCell = orderDetailsGrid.getRowDetailCellEl(row);
+                orderDetailsGrid.updateRow(row, {status: 2});
                 //console.log(row);
             };
         },
@@ -220,49 +230,73 @@ var CapcitySchedul = function(){
             }
             var param = {};
             var customerTaskFlowData = this.orderGrid.getSelected();
-            param.customerTaskFlowId = customerTaskFlowData.id;
+            if (customerTaskFlowData)
+            {
+                param.customerTaskFlowId = customerTaskFlowData.id;
+            }
+            else
+            {
+                mini.alert("请选择客户订单");
+                return;
+            }
             var plans = [];
             var submitData = orderDetailsGrid.getChanges();
             for (var i = 0; i < submitData.length; i++) {
                 var plansData = {};
-                if (submitData[i].dispatchInfoId) {
-                    plansData.dispatchInfoId = submitData[i].dispatchInfoId;
+                if (submitData[i].id) {
+                    plansData.id = submitData[i].id;
                 }
                 plansData.shipId = submitData[i].shipId;
                 plansData.preWeight = submitData[i].preWeight;
                 plansData.preLoad = submitData[i].preLoad;
                 plansData.actualTransferPrice = submitData[i].actualTransferPrice;
-                var prePaseDate = mini.parseDate(submitData[i].preArriveTime);
-                plansData.preArriveTime = this.funTimeFormat(mini.formatDate(prePaseDate, "yyyy-MM-dd HH:mm:ss"));
-                plansData.preSettleAmount = submitData[i].preSettleAmount;
+                if (submitData[i].id) {
+                    plansData.preArriveTime = submitData[i].preArriveTime
+                } else {
+                    var prePaseDate = mini.parseDate(submitData[i].preArriveTime);
+                    plansData.preArriveTime = this.funTimeFormat(mini.formatDate(prePaseDate, "yyyy-MM-dd HH:mm:ss"));
+                    plansData.preSettleAmount = submitData[i].preSettleAmount;
+                }
                 plansData.settleType = submitData[i].settleType;
-                if (submitData[i]._state == "added") { //flag: 1.新增  2.修改  3.删除
-                    plansData.flag = 1;
-                } else if (submitData[i]._state == "modified" && submitData[i].status != 3) {
-                    plansData.flag = 2;
-                } else if (submitData[i].status == 3) {
+                if (submitData[i]._state == "added") { //flag: 1.修改  2.删除  3.新增
                     plansData.flag = 3;
+                } else if (submitData[i]._state == "modified" && submitData[i].status != 2) {
+                    plansData.flag = 1;
+                } else if (submitData[i].status == 2) {
+                    plansData.flag = 2;
                 }
                 plans.push(plansData);
             }
             param.plans = plans;
 
             //orderDetailsGrid.loading("保存中，请稍后......");
-            PageMain.callAjax(PageMain.defaultOption.httpUrl + "/dispatch/confirmPlan", {customerTaskFlowId: customerTaskFlowData.id, plans: JSON.stringify(param.plans)}, function (data) {
-                if (data.success)
-                {
-                    mini.alert("操作成功", "提醒", function(){
-                        if(data.success)
-                        {
-                            location.reload();
-                        }
-                    });
-                }
-                else
-                {
-                    PageMain.funShowMessageBox(data.msg);
-                }
-            });
+            if (plans.length > 0)
+            {
+                mini.confirm("是否确定提交订单?", "提醒", function (action) {
+                    if (action == "ok")
+                    {
+                        PageMain.callAjax(PageMain.defaultOption.httpUrl + "/dispatch/confirmPlan", {"customerTaskFlowId": customerTaskFlowData.id, "plans": JSON.stringify(param.plans)}, function (data) {
+                            if (data.success)
+                            {
+                                mini.alert("操作成功", "提醒", function(){
+                                    if(data.success)
+                                    {
+                                        location.reload();
+                                    }
+                                });
+                            }
+                            else
+                            {
+                                PageMain.funShowMessageBox(data.msg);
+                            }
+                        });
+                    }
+                });
+            }
+            else
+            {
+                mini.alert("没有可以提交的数据");
+            }
         },
         /**
          * 乘法运算，避免数据相乘小数点后产生多位数和计算精度损失。
@@ -284,8 +318,7 @@ var CapcitySchedul = function(){
         },
         funTimeFormat: function(date)
         {
-            var formatDate = date.replace(new RegExp("-","gm"),"/");
-            var secondDate = (new Date(formatDate)).getTime() / 1000;
+            var secondDate = (new Date(date)).getTime() / 1000;
             return secondDate;
         }
     }
