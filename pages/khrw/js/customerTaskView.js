278aefd37d98aa2289c2a008285c2239ee59285c
litao <TeamMicLee@foxmail.com>
2018-06-20 15:18:16 +0800

    客户任务查看

--- pages/khrw/js/customerTaskView.js
+++ pages/khrw/js/customerTaskView.js
@@ -9,14 +9,22 @@ var PageCustomerTastView = function(){
         {
             mini.parse();
             this.basePath = PageMain.basePath;
-            this.customerTaskViewGrid = PageCustomerTask.defaultOption.customerTaskViewGrid;
-            //this.customerTaskViewGrid.setUrl(PageMain.defaultOption.httpUrl + "/customerTaskFlow/getList?id")
+            this.customerTaskViewGrid = mini.get("customerTaskViewGrid");
+            this.customerTaskViewGrid.setUrl(PageMain.defaultOption.httpUrl + "/customerTask/getTaskDetailList");
+            PageCustomerTastView.funSearch();
         },
         funSearch : function()
         {
         	var customerTaskViewForm = new mini.Form("customerTaskViewForm");
         	this.customerTaskViewGrid.load(customerTaskViewForm.getData());
-        }
+        },
+        funReset : function()
+        {
+            var customerTaskViewForm = new mini.Form("customerTaskViewForm");
+            customerTaskViewForm.setData();
+            mini.get("queryParamFlag").setValue("1");
+            this.customerTaskViewGrid.load(customerTaskViewForm.getData());
+        },
     }
 }();
 
