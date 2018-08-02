199b54f5e1e8e04398d97b42f8e8876f264eae16
1107323889 <1107323889@qq.com>
2018-08-02 22:01:06 +0800

    Merge branch 'master' of E:\webProject\web with conflicts.

--- jslib/miniui/jquery.gesoft-1.0.js
+++ jslib/miniui/jquery.gesoft-1.0.js
@@ -5,7 +5,7 @@ var PageMain = function(){
             isNenuFlag:false,//是否根据权限显示操作功能; true时不按数据库处理
             userProfileFly:[],
             zero:"0000000000000000000000000000000000",
-            httpUrl : window.location.protocol + "//" + window.location.host + "/xyl" //"http://xingyi.nandasoft-its.com:8080/xyl"//"http://xingyi.nandasoft-its.com:8080/xyl"//"http://127.0.0.1:16721/xyl"
+            httpUrl : "http://127.0.0.1:16722/xyl"
         },
         init :function (basePath){
             this.basePath = basePath;
