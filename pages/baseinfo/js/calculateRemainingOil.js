2973c22e3473f3240b434567d82fa49690936742
1107323889 <1107323889@qq.com>
2018-09-02 00:14:12 +0800

    剩余油量查询页面修改

--- pages/baseinfo/js/calculateRemainingOil.js
+++ pages/baseinfo/js/calculateRemainingOil.js
@@ -4,6 +4,8 @@ var PageShipOil = function(){
         defaultOption: {
             basePath:"",
             shipOilGrid : null,
+            shipNoData:[],
+            disIdData:[]
         },
         init :function ()
         {
@@ -11,11 +13,27 @@ var PageShipOil = function(){
             this.basePath = PageMain.basePath;
             this.shipOilGrid = mini.get("shipOilGrid");
             this.shipOilGrid.setUrl(PageMain.defaultOption.httpUrl + "/shipOil/calculateRemainingOil");
-            PageMain.callAjax(PageMain.defaultOption.httpUrl + "/shipOil/calculateRemainingOil",{pageSize:100000}, function (data) {
+            PageMain.callAjax(PageMain.defaultOption.httpUrl + "/ship/getList",{pageSize:100000}, function (data) {
                 PageShipOil.defaultOption.shipNoData = data.data.list;
                 PageShipOil.funSearch();
             });
+            PageMain.callAjax(PageMain.defaultOption.httpUrl + "/dispatch/loadDispatchInfo",{key:null}, function (data) {
+                PageShipOil.defaultOption.disIdData = data;
+                //mini.get("key").setData(data);
+            });
 
+        },
+        funShipIdRenderer : function (e)//船号转码
+        {
+            for(var nItem = 0; nItem < PageShipOil.defaultOption.shipNoData.length; nItem++)
+            {
+
+                if(e.value == PageShipOil.defaultOption.shipNoData[nItem].id)
+                {
+                    return PageShipOil.defaultOption.shipNoData[nItem].shipNo;
+                }
+            }
+            return e.value;
         },
         funSearch : function()
         {
